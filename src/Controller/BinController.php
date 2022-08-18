48428d5372aa76950603828ee6e17f67405cd54f
Martin <cressein.martin@gmail.com>
2022-08-18 11:23:46 +0200

    Affichage de la corbeille
    Ajout de la navbar globale

--- src/Controller/BinController.php
+++ src/Controller/BinController.php
@@ -30,6 +30,21 @@ class BinController extends AbstractController
     }
 
     /**
+     * @Route("/bin/restore/{id}", name="app_restore_one")
+     */
+    public function restoreOne(ManagerRegistry $doctrine, $id): RedirectResponse
+    {
+        $em = $doctrine->getManager();
+        $entity = $em->find(RecPSRTable::class ,$id);
+
+        $entity->setVisible(true);
+
+        $em->flush();
+
+        return $this->redirectToRoute('app_show_bin_filter', ['sortFilter' => 'origine']);
+    }
+
+    /**
      * @Route("/bin/empty", name="app_empty_psr")
      */
     public function empty(ManagerRegistry $doctrine): RedirectResponse
@@ -44,5 +59,37 @@ class BinController extends AbstractController
         $em->flush();
 
         return $this->redirectToRoute('app_show_recueil', ['sortFilter' => 'origine']);
+    }
+
+    /**
+     * @Route("/bin/empty/{id}", name="app_empty_one")
+     */
+    public function emptyOne(ManagerRegistry $doctrine, $id): RedirectResponse
+    {
+        $em = $doctrine->getManager();
+        $entity = $em->find(RecPSRTable::class ,$id);
+
+        $em->remove($entity);
+
+        $em->flush();
+
+        return $this->redirectToRoute('app_show_bin_filter', ['sortFilter' => 'origine']);
+    }
+
+    #[Route('/bin/{sortFilter}', name: 'app_show_bin_filter')]
+    public function showBinFilter($sortFilter, ManagerRegistry $doctrine)
+    {
+        $tables = $doctrine->getRepository(RecPSRTable::class)->findBy(['visible' => false]);
+        
+        return $this->render('bin/index.html.twig', [
+            'tables' => $tables,
+            'sortFilter' => $sortFilter,
+        ]);
+    }
+
+    #[Route('/bin', name: 'app_show_bin')]
+    public function showBin(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_show_bin_filter', ['sortFilter' => 'origine']);
     }
 }
